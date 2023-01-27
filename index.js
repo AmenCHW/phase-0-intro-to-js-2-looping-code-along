b674b523628a0fc489fded08885163636a2ce393
Ameen Abdulsamad <amenchawshly98@gmail.com>
2023-01-27 16:05:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    let cards= []
+    for(let i=0; i<names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+      }
+}
+let number = 10
+countDown(number)
